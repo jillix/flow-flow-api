bd3c4399ac50c60c097d5beaa0052688cb1d4aea
Ionică Bizău <bizauionica@gmail.com>
2016-02-23 20:41:36 +0200

    Expose all the flow-api methods

--- example/index.js
+++ example/index.js
@@ -1,12 +1,5 @@
 const flowApi = require("../lib");
 
-flowApi.getAppGraphData({}, {
-    app: "service"
-}, function (err, data) {
-    /* do something */
-});
-
-
 flowApi.searchModules({}, { pattern: "html" }, function (err, data) {
     console.log(err || data);
 });
